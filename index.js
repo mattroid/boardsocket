ad296d6aa68cc93bcc3673df50d6e76faea27f3d
David Konsumer <konsumer@jetboystudio.com>
2015-12-17 16:07:50 -0800

    color for server status

--- index.js
+++ index.js
@@ -12,19 +12,21 @@ var board = row.map(function(){ return row.slice() })
 board[0][0] = 1
 var player_position = {x:0, y:0}
 
+function updatePlayer(x, y){
+	console.log('click', x, y)
+	board[player_position.x][player_position.y] = 0
+	board[x][y] = 1
+	io.sockets.emit('board', 0, player_position.x, player_position.y)
+	io.sockets.emit('board', 1, x, y)
+	player_position.x = x
+	player_position.y = y
+}
+
 app.use(express.static('site'))
 
 io.on('connection', function (socket) {
   socket.emit('board', board)
-  socket.on('click', function(x, y){
-  	console.log('click', x, y)
-	board = row.map(function(){ return row.slice() })
-  	board[x][y] = 1
-  	io.sockets.emit('board', 0, player_position.x, player_position.y)
-  	io.sockets.emit('board', 1, x, y)
-  	player_position.x = x
-  	player_position.y = y
-  })
+  socket.on('click', updatePlayer)
 })
 
 var port = process.env.PORT || 8000
