3aa29403e0b2bc49b3efaa19bd11da21c10739c7
MATTHEW OCONNOR <matto@MATTHEWsMacBook.lan>
2015-12-17 23:52:14 -0800

    added concept of players, currently a 3 player game...

--- index.js
+++ index.js
@@ -9,24 +9,31 @@ var io = require('socket.io').listen(server)
 var row = []
 for (var i=10;i--;i){ row.push(0) }
 var board = row.map(function(){ return row.slice() })
-board[0][0] = 1
-var player_position = {x:0, y:0}
+//board[0][0] = 1
+var players = {'1':{x:0,y:0},'2':{x:1,y:1},'3':{x:2,y:2}}
+var playerNumbers = [1,2,3]
 
-function updatePlayer(x, y){
+function updatePlayer(x, y, playerNum){
+    if(playerNum==0) return;
 	console.log('click', x, y)
-	board[player_position.x][player_position.y] = 0
-	board[x][y] = 1
-	io.sockets.emit('board', 0, player_position.x, player_position.y)
-	io.sockets.emit('board', 1, x, y)
-	player_position.x = x
-	player_position.y = y
+	board[players[playerNum].x][players[playerNum].y] = 0
+	board[x][y] = playerNum
+	io.sockets.emit('board', 0, players[playerNum].x, players[playerNum].y)
+	io.sockets.emit('board', playerNum, x, y)
+	players[playerNum].x = x
+	players[playerNum].y = y
 }
 
 app.use(express.static('site'))
 
 io.on('connection', function (socket) {
   socket.emit('board', board)
-  socket.on('click', updatePlayer)
+    var num = playerNumbers.pop()
+    socket.emit('set_player_num', num)
+    socket.on('click', updatePlayer)
+    socket.on('disconnect', function() {
+	playerNumbers.push(num);
+    });
 })
 
 var port = process.env.PORT || 8000
