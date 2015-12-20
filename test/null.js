370c918e9e76ab804847cb0b9a00f22ad4210882
David Konsumer <konsumer@jetboystudio.com>
2015-12-20 10:44:01 -0800

    comment test utils

--- test/null.js
+++ test/null.js
@@ -1,3 +1,4 @@
-// ignore css requires
+// ignore css requires in tests with
+// mocha --compilers css:./test/null.js
 function noop () { return null }
 require.extensions['.css'] = noop
--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,5 @@
+// setup a fake DOM for tests with
+// mocha --require ./test/setup.js
 import jsdom from 'jsdom'
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.window = document.defaultView
