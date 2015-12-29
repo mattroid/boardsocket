979dc51cb34ad86b5190baee03951a3dfd287c44
MATTHEW OCONNOR <matto@MATTHEWsMacBook.lan>
2015-12-28 23:36:59 -0800

    added a boad test

--- src/components/Board/test.js
+++ src/components/Board/test.js
@@ -1,8 +1,18 @@
 import {describe, it} from 'mocha'
 import {assert} from 'chai'
 
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import Board from './index.js'
+
 describe('Board', () => {
-  it('should have testing', () => {
-    assert.ok('everything', 'everything is ok')
+  it('3x3 board should render 9 pieces', () => {
+    var player = {color: '#ababcc'}
+    var board = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+    var renderedComponent = TestUtils.renderIntoDocument(
+        <Board board={board} player={player} />
+    )
+    var tiles = TestUtils.scryRenderedDOMComponentsWithClass(renderedComponent, 'Piece')
+    assert.equal(tiles.length, 9)
   })
 })
